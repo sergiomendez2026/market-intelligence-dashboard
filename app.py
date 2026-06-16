d87383b201c3eaa1501d82b5fdd4a6cbd373ea30
sergiomendez2026 <smendez78910@gmail.com>
2026-06-16 08:14:20 -0500

    Refactor import statement for model module

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 from src.indicators import add_technical_indicators
 from src.data_loader import cargar_datos
 from src.features import create_ml_dataset, get_feature_columns
-from src.model import train_and_evaluate_model, train_and_evaluate_direction_model
+from src import model as market_model
 from src.signals import compute_market_signal
 
 
